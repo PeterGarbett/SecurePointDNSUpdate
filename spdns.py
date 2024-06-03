c44f1d6c08ded4753363d00fae02c21b6e451809
Peter Garbett <petegarbett1958@gmail.com>
2024-06-03 21:28:12 +0100

    Improved error handling and general tidy up

--- spdns.py
+++ spdns.py
@@ -1,117 +1,136 @@
 #!/usr/bin/python3
-#
-#    Update SecurePointDNS.  File should be on /usr/local/bin/
-#
-#
-import requests
-import json
+""" Update SecurePointDNS.  File should be on /usr/local/bin/ """
+
 import os
+import sys
+import requests
 from requests import get
 
 #
 #   Debug ensuring we don't pester the dns service
 #
-trialrun=False
+
+TRIAL_RUN = False
 
 
-# Load config file
-print("Loading SecurePointDNS config")
+def update_spdns_record():
+    """Find public ip and if its changed update spdns dns record"""
 
-# Config file is a single line with three parameters separated by a single space.
-# URL token IPFILE
-f = open(os.path.expanduser("/etc/spdns-config.conf"), "r")
+    # Load config file
+    print("Loading SecurePointDNS config")
 
-contents = f.readlines()
-f.close()
+    # Config file is a single line with three parameters separated by a single space.
+    # URL token IPFILE
 
-# expect a single line with info in it
-# and any number of comment lines to ignore
-# the real data line shouldn't have any #'s in it
+    config_file = open(
+        os.path.expanduser("/etc/spdns-config.conf"), "r", encoding="ascii"
+    )
+    contents = config_file.readlines()
+    config_file.close()
 
-for line in contents:
-    if "#" in line:
-        continue
-    else:
+    # Expect a single line with info in it
+    # and any number of comment lines to ignore
+    # the real data line shouldn't have any #'s in it
+
+    for line in contents:
+        if "#" in line:
+            continue
         conf = line
 
-toks = conf.strip().split(" ")
+    tokens = conf.strip().split(" ")
 
-# URL=toks[0] token=toks[2] IPFILE=toks[2]
+    # URL=tokens[0] token=tokens[2] IPFILE=tokens[2]
 
-ipfilename = toks[2]
+    ipfilename = tokens[2]
 
-# Make sure there is a file to record the IP into
+    # Make sure there is a file to record the IP into
 
-try:
-    ipfile = open(ipfilename, "r")
-except FileNotFoundError:
-    ipfile = open(ipfilename, "w")
-    ipfile.write("no ip")
-ipfile.close()
+    try:
+        ipfile = open(ipfilename, "r", encoding="ascii")
+    except FileNotFoundError:
+        ipfile = open(ipfilename, "w", encoding="ascii")
+        ipfile.write("no ip")
+        ipfile.close()
 
-# Retrieve what we think the current IP is
+    # Retrieve what we think the current IP is
 
-ipfile = open(ipfilename, "r")
-pubip = ipfile.readline()
-ipfile.close()
-
-# Now find out what IP we currently have
-
-print("Retrieving external IP address")
-try:
-    ip = get("https://api.ipify.org").content.decode("utf8")
-except:
-    print("Failed to get IP address")
-    quit()
-
-
-
-# If the IP has changed update our records
-# If not there is nothing at all to do
-
-if pubip != ip:
-    print("IP changed, update IP file")
-    ipfile = open(ipfilename, "w")
-    ipfile.write(ip)
+    ipfile = open(ipfilename, "r", encoding="ascii")
+    pubip = ipfile.readline()
     ipfile.close()
-else:
-    print("IP unchanged, quit")
-    quit()
 
-# The IP has changed so need to update spdns
+    # Now find out what IP we currently have
 
-print("IP: " + str(ip))
-print(
-    "Updating SecurePointDNS with hostname " + str(toks[0]) + ", token " + str(toks[1])
-)
-data = {"hostname": toks[0], "myip": ip}
+    print("Retrieving external IP address")
+    try:
+        public_ip = get("https://api.ipify.org").content.decode("utf8")
+    except:
+        print("Failed to get IP address")
+        sys.exit()
 
-if trialrun:
-    
-    print("dns update suppressed to develop and debug systemd timer/service")
-    r = "good"
-else:
-    resp = requests.get(
-    "https://update.spdyn.de/nic/update", params=data, auth=(toks[0], toks[1])
- )
-    r = resp.text.split(" ")[0]
+    # If the IP has changed update our records
+    # If not there is nothing at all to do
+
+    if pubip != public_ip:
+        print("IP changed, update IP file")
+        ipfile = open(ipfilename, "w", encoding="ascii")
+        ipfile.write(public_ip)
+        ipfile.close()
+    else:
+        print("IP unchanged, quit")
+        sys.exit()
+
+    # The IP has changed so need to update spdns
+
+    print("IP: " + str(public_ip))
+    print(
+        "Updating SecurePointDNS with hostname "
+        + str(tokens[0])
+        + ", token "
+        + str(tokens[1])
+    )
+    data = {"hostname": tokens[0], "myip": public_ip}
+
+    if TRIAL_RUN:
+        print(" SecurePointDNS update suppressed")
+        response = "good"
+    else:
+        resp = requests.get(
+            "https://update.spdyn.de/nic/update",
+            params=data,
+            auth=(tokens[0], tokens[1]),
+        )
+        response = resp.text.split(" ")[0]
+
+    print("response:", response)
+
+    rescodes = {
+        "abuse": "The host is locked because of too many failed attempts.",
+        "badauth": "The given username / token was not accepted",
+        "!yours": "The host could not be managed by your account",
+        "nochg": "Your IP has not changed since the last update",
+        "good": "IP of " + tokens[0] + " was updated to " + public_ip,
+        "notfqdn": "The host is not an FQDN",
+        "nohost": "The host does not exist or was deleted",
+        "fatal": "The host was manually deactivated",
+    }
+
+    # Decode the response
+
+    if response in rescodes:
+        print("Response: " + rescodes[response])
+    else:
+        print("Unknown response, " + response)
+
+    # If the update failed, make our idea of what the
+    # ip is 'wrong' so it will attempt to reset next time
+
+    if response == "good" or response == "nochg":
+        sys.exit()
+
+    ipfile = open(ipfilename, "w", encoding="ascii")
+    ipfile.write("ip update failed")
+    ipfile.close()
 
 
-rescodes = {
-    "abuse": "The host is locked because of too many failed attempts.",
-    "badauth": "The given username / token was not accepted",
-    "!yours": "The host could not be managed by your account",
-    "nochg": "Your IP has not changed since the last update",
-    "good": "IP of " + toks[0] + " was updated to " + ip,
-    "notfqdn": "The host is not an FQDN",
-    "nohost": "The host does not exist or was deleted",
-    "fatal": "The host was manually deactivated",
-}
-
-# Decode the response
-
-if r in rescodes:
-    print("Response: " + rescodes[r])
-else:
-    print("Unknown response, " + r)
-
+if __name__ == "__main__":
+    update_spdns_record()
