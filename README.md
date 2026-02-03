f1f74cae67251d7543908f85379af75974a49f9d
Peter Garbett <petegarbett1958@gmail.com>
2026-02-03 17:41:48 +0000

    Integrated with cloudns update code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-Scripts to update SecurePointDNS dns service to current public IP
+Scripts to update SecurePointDNS/cloudns  dns service to current public IP
 -----------------------------------------------------------------
 
 Files:
@@ -9,6 +9,10 @@ spdns.conf   	            Example configuration file (modify and place on /etc/)
 spdns.py                    Python to update spdyn when necessary
 spdns.service               systemd service file
 spdns.timer                 systemd timer file
+cloudns.service		    example systemd service file if using cloudns
+cloudns.timer               systemd timer file if using cloudns
+copy-script		    Called on ipfile update so you can process or copy it etc.
+setup-virt                  virtualenv, pip etc for dns and requests 
 
 
 The format is:
@@ -24,7 +28,13 @@ actually changed to minimise interactions with the dns service.
 It is possible to get false results where the ip isn't updated
 so theres a check via dns lookup is the ip looks unchanged
 
+Cloudns updates are now integrated with this code, the url
+needed to update can be passed on the command line, with
+examples cloudns.service and cloudns.timer 
+
 The spdns.py  file is run from /home/embed/SecurePointDNSUpdate/
 and and the systemd files on /etc/systemd/system/
 The usual incantations are required to enable and start the systemd timer  
+
+
 
--- spdns.py
+++ spdns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Update SecurePointDNS."""
+"""Update SecurePointDNS/cloudns ."""
 
 import os
 import os.path
@@ -16,7 +16,7 @@ TRIAL_RUN = False
 
 
 def name_to_ip(name):
-    """Find what ip address the nameservers have"""
+    """Find what ip address the nameservers have for name"""
     try:
         answers = dns.resolver.resolve(name, "A")
         for rdata in answers:
@@ -29,13 +29,12 @@ def name_to_ip(name):
 
 
 def spdns_ip_update(public_ip, site, ipfilename, tokens):
-
+    """spdns rigmarole and result decoding"""
     print("IP: " + str(public_ip))
     print(
         "Updating SecurePointDNS with hostname " + site + ", token " + str(ipfilename)
     )
     data = {"hostname": tokens[0], "myip": public_ip}
-    auth = (tokens[0], tokens[1])
 
     if TRIAL_RUN:
         print("SecurePointDNS update suppressed")
@@ -45,6 +44,7 @@ def spdns_ip_update(public_ip, site, ipfilename, tokens):
             "https://update.spdyn.de/nic/update",
             params=data,
             auth=(tokens[0], tokens[1]),
+            timeout=20,
         )
         response = resp.text.split(" ")[0]
 
@@ -77,7 +77,31 @@ def spdns_ip_update(public_ip, site, ipfilename, tokens):
     return False
 
 
-def update_spdns_record():
+def update_cloudns_record(dynamic_dns_url):
+    """This is the cloudns defined magic url trick to ask them to update"""
+
+    if not TRIAL_RUN:
+        try:
+            if sys.version_info[0] < 3:
+                import urllib
+
+                page = urllib.urlopen(dynamic_dns_url)
+                page.close()
+            else:
+                import urllib.request
+
+                page = urllib.request.urlopen(dynamic_dns_url)
+                page.close()
+        except Exception as err:
+            print(err)
+            return False
+    else:
+        print("Dummy call to update IP")
+
+    return True
+
+
+def update_spdns_record(cloudns_url):
     """Find public ip and if its changed update spdns dns record"""
 
     # Load config file (this would arguably be better down using json)
@@ -115,42 +139,22 @@ def update_spdns_record():
     except:
         script = ""
 
-    # Make sure there is a file to record the IP into
-
-    try:
-        ipfile = open(ipfilename, "r", encoding="ascii")
-    except FileNotFoundError:
-        ipfile = open(ipfilename, "w", encoding="ascii")
-        ipfile.write("Unknown")
-        ipfile.close()
-
-    # Retrieve what we think the current IP is
-    try:
-
-        ipfile = open(ipfilename, "r", encoding="ascii")
-        ipdata = ipfile.readline()
-        ip_and_site = ipdata.strip().split(" ")
-        pubip = ip_and_site[0]
-        print("IP from file:", pubip, " for ", site)
-        ipfile.close()
-    except Exception as err:
-        print("ERROR in IP file", err)
-        pubip = "Unknown"
-
-    # Now find out what IP we currently have
-
+    # Find out what IP we currently have
     # print("Retrieving external IP address")
+
     try:
-        public_ip = get("https://api.ipify.org").content.decode("utf8")
+        public_ip = get("https://api.ipify.org", timeout=20).content.decode("utf8")
     except:
         print("Failed to get IP address")
         sys.exit()
 
+    address = name_to_ip(site)
+
     # If the IP has changed update our records
     # If not there is nothing at all to do
 
-    if pubip != public_ip:
-        print("IP changed, update IP file")
+    if address != public_ip:
+        print("IP changed, update IP file and dns")
         ipfile = open(ipfilename, "w", encoding="ascii")
         line = public_ip + " " + site
         ipfile.write(line)
@@ -159,21 +163,37 @@ def update_spdns_record():
         if os.path.exists(script):
             os.system(script)
     else:
-        address = name_to_ip(site)
-        # print("nameservers return:",address," for ",site)
-        if address == public_ip:
-            print("IP unchanged, quit")
-            sys.exit()
+        print("IP unchanged, quit")
+        sys.exit()
 
-    # The IP has changed so need to update spdns
+    # The IP has changed so need to update cloudns/spdns
 
-    success = spdns_ip_update(public_ip, site, ipfilename, tokens)
+    if not cloudns_url:
+        print("use spdns")
+        success = spdns_ip_update(public_ip, site, ipfilename, tokens)
+        if not success:
+            ipfile = open(ipfilename, "w", encoding="ascii")
+            ipfile.write("ip update failed")
+            ipfile.close()
+    else:
+        print("use cloudns")
+        success = update_cloudns_record(cloudns_url)
+        if not success:
+            print("Cloudns update failed")
 
-    if not success:
-        ipfile = open(ipfilename, "w", encoding="ascii")
-        ipfile.write("ip update failed")
-        ipfile.close()
+
+def main():
+    """if using cloudns the url we need to look at to update our ip is passed as an argument"""
+    inputargs = sys.argv
+    sys.argv.pop(0)
+
+    if len(inputargs) == 1:
+        dynamic_dns_url = inputargs[0]
+    else:
+        dynamic_dns_url = ""
+
+    update_spdns_record(dynamic_dns_url)
 
 
 if __name__ == "__main__":
-    update_spdns_record()
+    main()
